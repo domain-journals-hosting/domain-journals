fc08dfd04987bec097c4f1ba6dd9efdcb5788cc5
Henry <Henries90@gmail.com>
2025-06-27 20:06:25 +0100

    Remove localhost

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // "https://api.domainjournals.com",
-  baseURL: "http://localhost:3500",
+  baseURL: "https://api.domainjournals.com",
+  //"http://localhost:3500",
   timeout: 20000,
   withCredentials: true,
 });
