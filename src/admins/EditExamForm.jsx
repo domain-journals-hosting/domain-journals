e3ab080f350741b1fb5fd8aa193188863ce65575
Henry <Henries90@gmail.com>
2025-10-10 09:03:28 +0100

    fixed lowercase f

--- src/admins/EditExamForm.jsx
+++ src/admins/EditExamForm.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "../api/axios";
 import QuestionInput from "./QuestionInput";
-import "../styles/examform.css";
+import "../styles/examForm.css";
 
 const EditExamForm = () => {
   const { courseId } = useParams();
