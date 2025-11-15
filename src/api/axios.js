92d3a745d7ec062c1d02735a1fab0538279d23ca
Henry <Henries90@gmail.com>
2025-11-15 23:48:46 +0100

    Added filters and support for matric, dept and level

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://10.186.146.156:3500",
-  baseURL: "http://localhost:3500",
-  // baseURL: "https://api.domainjournals.com",
+  // baseURL: "http://localhost:3500",
+  baseURL: "https://api.domainjournals.com",
 
   timeout: 20000,
   withCredentials: true,
